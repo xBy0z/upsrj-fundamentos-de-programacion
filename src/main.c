48df7c384922ac19df6c6641abf091d833310048
xBy0z <equinoxskrillex0@gmail.com>
2026-01-09 13:37:17 -0600

    entrega 1

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
+int main()
 {
-    printf("Hello world!\n");
+    int x = 100;
+    char letra_a = 'a';
+    float y = 3.14;
+    int z = 50;
+    int suma = x + y + z;
+
+    printf("x vale:  %i, letra_a: %c, y vale: %f, suma: %i\n", x, letra_a, y, suma);
     return 0;
+
 }
